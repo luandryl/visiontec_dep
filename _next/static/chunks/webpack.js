18f8b6935f9c5198e5c60aa479d335e5561e9bb4
Luan Andryl <luan.andryl@gmail.com>
2021-04-30 17:27:44 -0300

    quick fix

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "686f5fcca480938875dd";
+/******/ 	var hotCurrentHash = "5b40b988765dcf17ae08";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
