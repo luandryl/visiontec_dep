e480227ef29f35955bf1973e5b56ebb7a440e19d
Luan Andryl <luan.andryl@gmail.com>
2021-05-26 15:24:56 -0300

    v13

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "7d1bc6dc2b158df47639";
+/******/ 	var hotCurrentHash = "4133ee4f7c84927eb2eb";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
